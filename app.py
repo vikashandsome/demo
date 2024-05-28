04eac60dbd9de4c049c1d7d2598c8f12f87a40e8
vikashandsome <vcvikaschoudhary556@gmail.com>
2024-05-28 17:36:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = ''
-    if request.method == 'POST':
-        name = request.form.get('name', '')
-    return render_template('index.html', name=name)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
